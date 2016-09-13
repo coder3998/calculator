4f25420b2d55a715f0da84ecd42af15bf32e2ff0
sagar sinha <sagar sinha>
2016-09-14 02:48:37 +0530

    rough diagram of sagar's cal

--- calculator.java
+++ calculator.java
@@ -7,21 +7,60 @@ public static void main(String arr[])
 System.out.println("\t\t\t. . . . . .");
 System.out.println("\t\tSagar's calculator");
 
+subCal ob = new subCal();
+
 //type of calculator.
 
 Console c = System.console();
-String input = c.readLine("\t\t1. Simple calculator.\n\t\t2. Scientific calculator. ");
+String input = c.readLine("\n\n\t\t1. Simple calculator.\n\t\t2. Scientific calculator.\n\n\t\t");
 int x = Integer.parseInt(input);
 
 //choice...
 
 if(x==1){
-simple();}
+ob.simple();}
 
-if(x==2){
-scientific();}
+//if(x==2){
+//ob.scientific();}
+else
+{;}
+}}
 
-//if user enters other than choice provided....
-
-try
+class subCal
 {
+void simple()
+{
+Console v = System.console();
+System.out.println("\t\tSimple calculator");
+System.out.println();
+ 
+String input1 = v.readLine("\n\t\tX = ");
+int y = Integer.parseInt(input1);
+String input2 = v.readLine("\n\t\tY = ");
+int z = Integer.parseInt(input2);
+
+
+String input0 = v.readLine("\n\t\t1. Addition\n\t\t2. Subtraction\n\t\t3. Division\n\t\t4. Multiplication\n\t\t");
+int choice = Integer.parseInt(input0);
+
+switch(choice)
+{
+case 1 : 
+	System.out.println("\n\t\tAddition");
+	System.out.println("\n\t\t "+y+" +"+" "+z+" = "+(y+z));
+	break;
+case 2 :
+	System.out.println("\n\t\tSubtraction");
+	System.out.println("\n\t\t "+y+"-"+" "+z+" = "+(y-z));
+	break;
+case 3 :
+	System.out.println("\n\t\tDivision");
+	System.out.println("\n\t\t "+y+"/"+" "+" = "+(y/z));
+	break;
+case 4 :
+	System.out.println("\n\t\tMultiplication");
+	System.out.println("\n\t\t "+y+"*"+" "+"="+(y*z));
+default :
+	System.out.println("\n\t\tInvalid input");
+}
+}}
